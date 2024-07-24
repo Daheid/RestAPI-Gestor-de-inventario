2cdcf9323a805ea96e306a89d0cb03e167f7b0d0
Nelson <soynelsond@gmail.com>
2024-07-24 14:12:43 -0400

    creado el middleware apra verificar el token de jwt el login

---
+++ api/v1/scripts/verificardorToken.js
@@ -0,0 +1,22 @@
+const Usuario = require("./../models/usuarios")
+const jwt = require("jsonwebtoken")
+
+const verificarToken = async (req, res, next) => {
+  try {
+    const token = req.headers["x-access-token"]
+
+    if (!token) return res.status(400).json({ error: "No tiene sesion activa" })
+
+    const decoded = jwt.verify(token, proccess.env.TOKEN)
+
+    req.idUser = dedoced.id
+
+    const user = await Usuario.findById(req.idUser, { password: 0 })
+
+    if (!user) return res.status(404).json({ error: "El susuario no existe" })
+
+    next()
+  } catch (err) {
+    return res.status(500).json({ error: err })
+  }
+}
