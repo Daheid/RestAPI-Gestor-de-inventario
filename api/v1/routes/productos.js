aa28eb74e6e603054a32f4dec5a42c856e72a1e5
Nelson <soynelsond@gmail.com>
2024-08-02 11:39:50 -0400

    Realizado verificador de token y asignado a la ruta para obtener todos los productos

--- api/v1/routes/productos.js
+++ api/v1/routes/productos.js
@@ -1,9 +1,10 @@
 var express = require("express")
 var router = express.Router()
+const verificarToken = require("./../scripts/verificardorToken")
 
 //middleware
 const { Producto } = require("./../controllers/obtenerProductosController")
 
-router.get("/", Producto)
+router.get("/", verificarToken, Producto)
 
 module.exports = router
