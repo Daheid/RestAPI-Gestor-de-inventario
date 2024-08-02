58c39057d1af4acf0c8191b7df6b91866fbfc7dc
Nelson <soynelsond@gmail.com>
2024-08-02 17:57:52 -0400

    creada la ruta para la creacion de productos

--- api/v1/routes/productos.js
+++ api/v1/routes/productos.js
@@ -5,6 +5,12 @@ const verificarToken = require("./../scripts/verificardorToken")
 //middleware
 const { Producto } = require("./../controllers/obtenerProductosController")
 
+//EndPoint Obtener todos los productos
 router.get("/", verificarToken, Producto)
+
+//EndPoint Crear productos
+router.post("/", (req, res, next) => {
+  res.json({ messege: "aqui se crean productos" })
+})
 
 module.exports = router
