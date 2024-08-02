380a070d5adaaf2e3603eee588920c79c0e993b0
Nelson <soynelsond@gmail.com>
2024-08-02 18:24:12 -0400

    agregado callback en la ruta de productos

--- api/v1/routes/productos.js
+++ api/v1/routes/productos.js
@@ -4,13 +4,12 @@ const verificarToken = require("./../scripts/verificardorToken")
 
 //middleware
 const { Producto } = require("./../controllers/obtenerProductosController")
+const crearProducto = require("./../controllers/crearProductoController")
 
 //EndPoint Obtener todos los productos
 router.get("/", verificarToken, Producto)
 
 //EndPoint Crear productos
-router.post("/", (req, res, next) => {
-  res.json({ messege: "aqui se crean productos" })
-})
+router.post("/", crearProducto)
 
 module.exports = router
