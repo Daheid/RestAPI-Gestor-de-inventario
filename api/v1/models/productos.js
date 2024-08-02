6b66caa8a279b3551f68f333fdbb6c4cf92cf345
Nelson <soynelsond@gmail.com>
2024-08-02 12:17:19 -0400

    Documentacion del modelo de productos completada

--- api/v1/models/productos.js
+++ api/v1/models/productos.js
@@ -8,3 +8,35 @@ const productoSchema = new mongoose.Schema({
 })
 
 module.exports = mongoose.model("Productos", productoSchema)
+
+//Documentacion del Modelo de producto
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *     Productos:
+ *      type: object
+ *      properties:
+ *        nombre:
+ *          type: string
+ *          descripcion: Primer Nombre del producto
+ *        precio:
+ *          type: number
+ *          descripcion: Precio del producto
+ *        cantidad:
+ *          type: number
+ *          descripcion: Cantidad existente del producto
+ *        descripcion:
+ *          type: string
+ *          descripcion: Descripcion del producto
+ *      required:
+ *        - nombre
+ *        - precio
+ *        - cantidad
+ *        - descripcion
+ *      example:
+ *        nombre: "Camisa roja."
+ *        precio: 12
+ *        cantidad: 4
+ *        descripcion: "Camisa color rojo, talla M."
+ */
