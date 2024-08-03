e2524cae5464335f5d6fd20e1d8d31967d0cc000
Nelson <soynelsond@gmail.com>
2024-08-03 12:14:08 -0400

    creado el controlador para actualizar el prodcuto mediante su id

---
+++ api/v1/controllers/actualizarProductoController.js
@@ -0,0 +1,27 @@
+const Producto = require("./../models/productos")
+
+module.exports = async (req, res, next) => {
+  try {
+    if (!req.body || Object.keys(req.body).length === 0) {
+      return res.status(400).json({ mensaje: "No se recibieron datos." })
+    }
+
+    console.log("ID del producto a actualizar:", req.params.id)
+    const actualizado = await Producto.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    )
+
+    if (!actualizado) {
+      return res
+        .status(404)
+        .json({ mensaje: "No se encontro el producto indicado" })
+    }
+
+    res.status(200).json({ mensaje: "producto actualizado", actualizado })
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ error: "error de servidor" })
+  }
+}
