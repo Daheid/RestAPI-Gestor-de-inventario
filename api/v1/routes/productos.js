03f23c5994bf6c0ce5952a9cbadd81d4d05c3b18
Nelson <soynelsond@gmail.com>
2024-08-19 12:57:43 -0400

    inicializada la ruta deeliminar varios productos, tambien se le agrego la verificacion del token

--- api/v1/routes/productos.js
+++ api/v1/routes/productos.js
@@ -9,6 +9,7 @@ const {
 const crearProducto = require("./../controllers/productos/crearProductoController")
 const actualizarProducto = require("./../controllers/productos/actualizarProductoController")
 const eliminarProducto = require("./../controllers/productos/eliminarProductoController")
+const eliminarVariosProductos = require("./../controllers/productos/EliminarVariosProductosController")
 
 //EndPoint Obtener todos los productos
 router.get("/", verificarToken, Producto)
@@ -21,4 +22,7 @@ router.put("/:id", verificarToken, actualizarProducto)
 
 //EndPoint Eliminar productos
 router.delete("/:id", verificarToken, eliminarProducto)
+
+//EndPoint Eliminar varios productos
+router.delete("/", verificarToken, eliminarVariosProductos)
 module.exports = router
